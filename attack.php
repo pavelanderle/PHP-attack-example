abad6175a68f5b03e3d8940510fe60b5ce2716bd
Pavel Anderle <anderle@spseplzen.cz>
2020-09-27 14:59:26 +0200

    add list emails

--- attack.php
+++ attack.php
@@ -1,7 +1,7 @@
 <?php
     
-    if (isset($_GET["email"])) {
-        $email = $_GET["email"];
+    
+        
     $con = new mysqli("localhost","root","","attackphp");
 
     if ($con->connect_error) {
@@ -10,18 +10,36 @@
 
     $sql="SET CHARACTER SET UTF8";
     $con->query($sql);
+    if (isset($_GET["email"])) {
+        $email = $_GET["email"];
+        $sql="INSERT INTO email_adress (email) VALUE ('$email')";
 
-    $sql="INSERT INTO email_adress (email) VALUE ('$email')";
+        if ($con->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
 
-    if ($con->query($sql) === TRUE) {
-        echo "New record created successfully";
+    }
+
+    $sql = "SELECT * FROM email_adress";
+
+    $result=$con->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr><th>Id</th><th>Email</th></tr>";
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>".$row["id"]."</td><td>".$row["email"]."</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
       } else {
-        echo "Error: " . $sql . "<br>" . $con->error;
+        echo "0 results";
       }
 
-    $con->query($sql);
 
-    $con->close();
-    }
+
     
 
