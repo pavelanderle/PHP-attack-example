561cbc94fe9ad2703e4c75f545564784933a5b9d
Pavel Anderle <anderle@spseplzen.cz>
2020-09-28 21:54:39 +0200

    Complete

--- attack.php
+++ attack.php
@@ -2,7 +2,7 @@
     
     
         
-    $con = new mysqli("localhost","root","","attackphp");
+    $con = new mysqli("localhost","root","root","attackphp");
 
     if ($con->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -10,8 +10,8 @@
 
     $sql="SET CHARACTER SET UTF8";
     $con->query($sql);
-    if (isset($_GET["email"])) {
-        $email = $_GET["email"];
+    if (isset($_GET["addemail"])) {
+        $email = $_GET["addemail"];
         $sql="INSERT INTO email_adress (email) VALUE ('$email')";
 
         if ($con->query($sql) === TRUE) {
@@ -22,7 +22,18 @@
 
     }
 
-    $sql = "SELECT * FROM email_adress";
+    var_dump(!empty($_GET["findemail"]));
+
+    if (isset($_GET["findemail"]) && !empty($_GET["findemail"])) {
+        $findemail = $_GET["findemail"];
+        //$sql = "SELECT id, email FROM email_adress WHERE email='".$con->real_escape_string ($findemail)."'"; // ošetřeno
+        $sql = "SELECT id, email FROM email_adress WHERE email='$findemail'"; // neošetřeno
+        echo $sql;
+    }
+    else{
+        $sql = "SELECT id, email FROM email_adress";
+    }
+    
 
     $result=$con->query($sql);
 
@@ -31,7 +42,8 @@
         echo "<tr><th>Id</th><th>Email</th></tr>";
         while($row = $result->fetch_assoc()) {
             echo "<tr>";
-            echo "<td>".$row["id"]."</td><td>".$row["email"]."</td>";
+            //echo "<td>".htmlspecialchars($row["id"])."</td><td>".htmlspecialchars($row["email"])."</td>"; // ošetřeno
+            echo "<td>".$row["id"]."</td><td>".$row["email"]."</td>"; // neošetřeno
             echo "</tr>";
         }
         echo "</table>";
